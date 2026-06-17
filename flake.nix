9170ebc7363ecd1b7dc611e0765d10811dda799f
fuutot <153519467+fuutot@users.noreply.github.com>
2026-06-18 00:45:19 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "React development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in
+  {
+    devShells.${system}.default =
+      pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_26
+        ];
+
+        shellHock = ''
+          npm install
+        ''
+      };
+  };
+}
