a83345d3e54ca9128c4d1377d8efa2cc957ecab2
fuutot <153519467+fuutot@users.noreply.github.com>
2026-06-19 21:11:43 +0900

    format: prettierでプロジェクト全体をフォーマット

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useState, useRef } from 'react';
-import './App.css'
+import './App.css';
 
 const IS_DEBUG = import.meta.env.DEV;
 
 const WORK_TIME = IS_DEBUG ? 5 : 25 * 60; // 5 seconds for debug, 25 minutes for production
-
 
 function PomodoroTimer() {
   const [secondsLeft, setSecondsLeft] = useState(WORK_TIME);
@@ -12,7 +11,7 @@ function PomodoroTimer() {
 
   function startTimer() {
     intervalId.current = setInterval(() => {
-      setSecondsLeft(prevSecondsLeft => {
+      setSecondsLeft((prevSecondsLeft) => {
         // Updater 関数の中なら，最新の state を参照できる．
         if (prevSecondsLeft > 0) {
           return prevSecondsLeft - 1;
@@ -27,15 +26,20 @@ function PomodoroTimer() {
   return (
     <div>
       <div>
-        <span>{Math.floor(secondsLeft / 60)}</span>:<span>{secondsLeft % 60}</span>
+        <span>{Math.floor(secondsLeft / 60)}</span>:
+        <span>{secondsLeft % 60}</span>
       </div>
       <div>
-        <button onClick={ startTimer }>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button onClick={() => clearInterval(intervalId.current)}>Pause</button>
-        <button onClick={() => {
-          clearInterval(intervalId.current);
-          setSecondsLeft(WORK_TIME);
-        }}>Reset</button>
+        <button
+          onClick={() => {
+            clearInterval(intervalId.current);
+            setSecondsLeft(WORK_TIME);
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
@@ -48,7 +52,7 @@ function App() {
         <PomodoroTimer />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
