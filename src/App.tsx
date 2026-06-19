0c2253dd6e1d77ec1fddd904ce5b74ddcf76b9d4
fuutot <153519467+fuutot@users.noreply.github.com>
2026-06-19 20:54:11 +0900

    add: Startボタンのイベントハンドラを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,25 @@ import './App.css'
 
 const IS_DEBUG = import.meta.env.DEV;
 
-const WORK_TIME = IS_DEBUG ? 10 : 25 * 60; // 10 seconds for debug, 25 minutes for production
+const WORK_TIME = IS_DEBUG ? 5 : 25 * 60; // 5 seconds for debug, 25 minutes for production
 
 
 function PomodoroTimer() {
   const [secondsLeft, setSecondsLeft] = useState(WORK_TIME);
+
+  function startTimer() {
+    const intervalId = setInterval(() => {
+      setSecondsLeft(prevSecondsLeft => {
+        // Updater 関数の中なら，最新の state を参照できる．
+        if (prevSecondsLeft > 0) {
+          return prevSecondsLeft - 1;
+        } else {
+          clearInterval(intervalId);
+          return 0;
+        }
+      });
+    }, 1000);
+  }
 
   return (
     <div>
@@ -15,7 +29,7 @@ function PomodoroTimer() {
         <span>{Math.floor(secondsLeft / 60)}</span>:<span>{secondsLeft % 60}</span>
       </div>
       <div>
-        <button>Start</button>
+        <button onClick={ startTimer }>Start</button>
         <button>Pause</button>
         <button>Reset</button>
       </div>
